4dd6bca8ac0ee4e944c300e4690ce5022d1a1df1
xLazuriax <xLazuriax@users.noreply.github.com>
2019-05-28 10:34:40 +0200

    Modifications de l'ajout d'un film, acteurs, réalisateurs

--- controller/ajouter.php
+++ controller/ajouter.php
@@ -11,9 +11,97 @@ $annee=$_POST['annee'];
 $synopsis = $_POST['synopsis'];
 $url_affiche = $_POST['url_affiche'];
 
-$pays = $_POST['pays'];
+if (isset($_POST['autre']) && (!EMPTY ($_POST['autre']))){
+    $pays = $_POST['autre'];
+    $stmt = $connection->prepare("INSERT INTO `pays` (`nom`) VALUES (:autre)");
+    $stmt->bindParam(':autre', $pays);
+    $stmt->execute();
+}else{
+    $pays = $_POST['pays'];
+}
+
+
+
+
+if (isset($_POST['autreReal'])&& (!EMPTY ($_POST['autreReal']))){
+    $realisateur = $_POST['autreReal'];
+
+}else{
+    $realisateur = $_POST['realisateur'];
+}
+
+if (isset($_POST['autrePaysReal'])&& (!EMPTY ($_POST['autrePaysReal']))){
+    $paysReal = $_POST['autrePaysReal'];
+    $stmt = $connection->prepare("INSERT INTO `pays` (`nom`) VALUES (:autrePaysReal)");
+    $stmt->bindParam(':autrePaysReal', $paysReal);
+    $stmt->execute();
+}else{
+    $paysReal = $_POST['paysReal'];
+}
+
+
+
+
+if (isset($_POST['autreActeur'])&& (!EMPTY ($_POST['autreActeur']))){
+    $acteur = $_POST['autreActeur'];
+}else{
+    $acteur = $_POST['acteur'];
+}
+
+if (isset($_POST['autrePaysActeur'])&& (!EMPTY ($_POST['autrePaysActeur']))){
+    $paysActeur = $_POST['autrePaysActeur'];
+    $stmt = $connection->prepare("INSERT INTO `pays` (`nom`) VALUES (:autrePaysActeur)");
+    $stmt->bindParam(':autrePaysActeur', $paysActeur);
+    $stmt->execute();
+}else{
+    $paysActeur = $_POST['paysActeur'];
+}
+
+
+
+
+if (isset($_POST['autreActeur2'])&& (!EMPTY ($_POST['autreActeur2']))){
+    $acteur2 = $_POST['autreActeur2'];
+}else{
+    $acteur2 = $_POST['acteur2'];
+}
+
+if (isset($_POST['autrePaysActeur2'])&& (!EMPTY ($_POST['autrePaysActeur2']))){
+    $paysActeur2 = $_POST['autrePaysActeur2'];
+    $stmt = $connection->prepare("INSERT INTO `pays` (`nom`) VALUES (:autrePaysActeur2)");
+    $stmt->bindParam(':autrePaysActeur2', $paysActeur2);
+    $stmt->execute();
+}else{
+    $paysActeur2 = $_POST['paysActeur2'];
+}
+
+
+
+
+if (isset($_POST['autreActeur3'])&& (!EMPTY ($_POST['autreActeur3']))){
+    $acteur3 = $_POST['autreActeur3'];
+}else{
+    $acteur3 = $_POST['acteur3'];
+}
+
+if (isset($_POST['autrePaysActeur3'])&& (!EMPTY ($_POST['autrePaysActeur3']))){
+    $paysActeur3 = $_POST['autrePaysActeur3'];
+    $stmt = $connection->prepare("INSERT INTO `pays` (`nom`) VALUES (:autrePaysActeur3)");
+    $stmt->bindParam(':autrePaysActeur3', $paysActeur3);
+    $stmt->execute();
+}else{
+    $paysActeur3 = $_POST['paysActeur3'];
+}
+
+
+
+
+
+
+
 
 $selectPays = "SELECT id FROM pays WHERE nom = '". $pays ."'";
+
 
 $stmt = $connection->prepare("INSERT INTO `film` (`titre`, `annee`, `synopsis`, `url_affiche`, `pays_id`) VALUES (:titre, :annee, :synopsis, :url_affiche,(". $selectPays. "))");
 
@@ -26,6 +114,78 @@ $stmt->bindParam(':url_affiche', $url_affiche);
 
 
 $stmt->execute();
+
+
+
+
+$selectPaysReal = "SELECT id FROM pays WHERE nom = '". $paysReal ."'";
+
+$stmt = $connection->prepare("INSERT INTO `personne` (`full_name`, `pays_id`) VALUES (:full_name,(". $selectPaysReal. "))");
+
+$stmt->bindParam(':full_name', $realisateur);
+
+$stmt->execute();
+
+
+
+
+
+
+$selectPaysActeur = "SELECT id FROM pays WHERE nom = '". $paysActeur ."'";
+
+$stmt = $connection->prepare("INSERT INTO `personne` (`full_name`, `pays_id`) VALUES (:full_name,(". $selectPaysActeur. "))");
+
+$stmt->bindParam(':full_name', $acteur);
+
+$stmt->execute();
+
+
+
+
+$selectPaysActeur2 = "SELECT id FROM pays WHERE nom = '". $paysActeur2 ."'";
+
+$stmt = $connection->prepare("INSERT INTO `personne` (`full_name`, `pays_id`) VALUES (:full_name,(". $selectPaysActeur2. "))");
+
+$stmt->bindParam(':full_name', $acteur2);
+
+$stmt->execute();
+
+
+
+
+$selectPaysActeur3 = "SELECT id FROM pays WHERE nom = '". $paysActeur3 ."'";
+
+$stmt = $connection->prepare("INSERT INTO `personne` (`full_name`, `pays_id`) VALUES (:full_name,(". $selectPaysActeur3. "))");
+
+$stmt->bindParam(':full_name', $acteur3);
+
+$stmt->execute();
+
+
+
+$selectFilm = "SELECT id FROM film WHERE titre = '". $titre ."'";
+
+$selectReal = "SELECT id FROM personne WHERE full_name = '". $realisateur ."'";
+
+$selectActeur = "SELECT id FROM personne WHERE full_name = '". $acteur ."'";
+
+$selectActeur2 = "SELECT id FROM personne WHERE full_name = '". $acteur2 ."'";
+
+$selectActeur3 = "SELECT id FROM personne WHERE full_name = '". $acteur3 ."'";
+
+
+$stmt = $connection->prepare("INSERT INTO `participe` (`personne_id`, `role_id`, `film_id`) VALUES ((" . $selectReal ."), (1),(". $selectFilm. "))");
+$stmt->execute();
+
+$stmt = $connection->prepare("INSERT INTO `participe` (`personne_id`, `role_id`, `film_id`) VALUES ((" . $selectActeur ."), (2),(". $selectFilm. "))");
+$stmt->execute();
+
+$stmt = $connection->prepare("INSERT INTO `participe` (`personne_id`, `role_id`, `film_id`) VALUES ((" . $selectActeur2 ."), (2),(". $selectFilm. "))");
+$stmt->execute();
+
+$stmt = $connection->prepare("INSERT INTO `participe` (`personne_id`, `role_id`, `film_id`) VALUES ((" . $selectActeur3 ."), (2),(". $selectFilm. "))");
+$stmt->execute();
+
 
 header('location:../index.php');
 
--- pages/formulaire.php
+++ pages/formulaire.php
@@ -2,40 +2,171 @@
 
 require_once ('./common/connexionBase.php');
 
-$stmt = $connection->prepare("SELECT * FROM `pays`");
+$stmt = $connection->prepare("SELECT * FROM `pays` ORDER BY nom asc ");
 $stmt->execute();
 $lesPays = $stmt->fetchall();
 
+$stmt2 = $connection->prepare("SELECT * FROM `personne` ORDER BY full_name asc");
+$stmt2->execute();
+$lesPersonnes = $stmt2->fetchall();
 
 ?>
 
 
 
+<div class="container blocPrincipalForm d-flex align-items-center">
+    <div class="row ">
+
+        <form class="row d-flex justify-content-center" action="./controller/ajouter.php" method="POST">
+            <input type="hidden" name="id" value="">
+
+            <div class="col-12 text-white d-flex justify-content-center">
+                <h2>Le film</h2>
+            </div>
+            <!--                <label for="titre">Titre :</label>-->
+            <input class="col-5" type="text" name="titre" id="titre" placeholder="Titre" value="">
 
 
-<form action="./controller/ajouter.php" method="POST">
-    <input type="hidden" name="id" value="">
+            <!--                <label for="annee">Année :</label>-->
+            <input class="col-5" type="text" name="annee" id="annee" placeholder="Année" value="">
 
-    <label for="titre">Titre :</label>
-    <input type="text" name="titre" id="titre" value="">
 
-    <label for="annee">Année :</label>
-    <input type="text" name="annee" id="annee" value="">
+            <!--                <label for="synopsis">Synopsis :</label>-->
+            <input class="col-5" type="text" name="synopsis" id="synopsis" placeholder="Synopsis" value="">
 
-    <label for="synopsis">Synopsis :</label>
-    <input type="text" name="synopsis" id="synopsis" value="">
 
-    <label for="url_affiche">Url Affiche :</label>
-    <input type="text" name="url_affiche" id="url_affiche" value="">
+            <!--                <label for="url_affiche">Url Affiche :</label>-->
+            <input class="col-5" type="text" name="url_affiche" id="url_affiche" placeholder="Affiche" value="">
 
-    <label for="pays">Nom :</label>
-    <select  name="pays" id="pays">
-        <?php
-        foreach ($lesPays as $pays){
-            echo '<option value="' . $pays["nom"] . '">' . $pays["nom"] . '</option>';
-        }
-        ?>
-    </select>
-    <input type="submit" value="Ajouter">
 
-</form>
+            <!--            <label for="pays">Nom :</label>-->
+            <select  class="col-5" name="pays" id="pays">
+                <?php
+                foreach ($lesPays as $pays){
+                    echo '<option value="' . $pays["nom"] . '">' . $pays["nom"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autre" id="autre" placeholder="Ou autre pays">
+
+
+
+
+
+
+            <div class="col-12 text-white d-flex justify-content-center">
+                <h2>Réalisateur</h2>
+            </div>
+
+            <select  class="col-5" name="realisateur" id="realisateur">
+                <?php
+                foreach ($lesPersonnes as $realisateur){
+                    echo '<option value="' . $realisateur["full_name"] . '">' . $realisateur["full_name"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autreReal" id="autreReal" placeholder="Ou autre réalisateur">
+
+            <select  class="col-5" name="paysReal" id="paysReal">
+                <?php
+                foreach ($lesPays as $pays){
+                    echo '<option value="' . $pays["nom"] . '">' . $pays["nom"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autrePaysReal" id="autrePaysReal" placeholder="Ou autre pays">
+
+
+
+
+
+
+
+
+            <div class="col-12 text-white d-flex justify-content-center">
+                <h2>Acteur principal</h2>
+            </div>
+
+            <select  class="col-5" name="acteur" id="acteur">
+                <?php
+                foreach ($lesPersonnes as $acteur){
+                    echo '<option value="' . $acteur["full_name"] . '">' . $acteur["full_name"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autreActeur" id="autreActeur" placeholder="Ou autre acteur">
+
+            <select  class="col-5" name="paysActeur" id="paysActeur">
+                <?php
+                foreach ($lesPays as $pays){
+                    echo '<option value="' . $pays["nom"] . '">' . $pays["nom"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autrePaysActeur" id="autrePaysActeur" placeholder="Ou autre pays">
+
+            <div class="col-12 text-white d-flex justify-content-center">
+                <h2>Acteurs secondaires</h2>
+            </div>
+
+
+            <select  class="col-5" name="acteur2" id="acteur2">
+                <?php
+                foreach ($lesPersonnes as $acteur2){
+                    echo '<option value="' . $acteur2["full_name"] . '">' . $acteur2["full_name"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autreActeur2" id="autreActeur2" placeholder="Ou autre Acteur">
+
+            <select  class="col-5" name="paysActeur2" id="paysActeur2">
+                <?php
+                foreach ($lesPays as $pays){
+                    echo '<option value="' . $pays["nom"] . '">' . $pays["nom"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autrePaysActeur2" id="autrePaysActeur2" placeholder="Ou autre pays">
+
+
+
+
+
+
+            <select  class="col-5" name="acteur3" id="acteur3">
+                <?php
+                foreach ($lesPersonnes as $acteur3){
+                    echo '<option value="' . $acteur3["full_name"] . '">' . $acteur3["full_name"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autreActeur3" id="autreActeur3" placeholder="Ou autre réalisateur">
+
+            <select  class="col-5" name="paysActeur3" id="paysActeur3">
+                <?php
+                foreach ($lesPays as $pays){
+                    echo '<option value="' . $pays["nom"] . '">' . $pays["nom"] . '</option>';
+                }
+                ?>
+
+            </select>
+            <input class="col-5" type="text" name="autrePaysActeur3" id="autrePaysActeur3" placeholder="Ou autre pays">
+
+
+
+
+            <input class="col-3" type="submit" value="Ajouter">
+
+
+
+        </form>
+    </div>
+</div>
